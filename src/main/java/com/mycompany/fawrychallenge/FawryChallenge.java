ef31654870bad63e003fe538d0952c79a6769b7e
mohamed ahmad <amohamedahmad68@gmail.com>
2025-07-04 18:26:19 +0300

    add product and Customer

---
+++ src/main/java/com/mycompany/fawrychallenge/FawryChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fawrychallenge;
+
+/**
+ *
+ * @author mosa
+ */
+public class FawryChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/fawrychallenge/Product.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.fawrychallenge;
+
+public class Product {
+    String name;
+    double price;
+    int quantity;
+    boolean expire;
+}
